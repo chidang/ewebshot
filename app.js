f113cb2a2c7f219c8fdbe23515cc7bf67e990b1f
Dang Chi <chid@jobready.com.au>
2019-05-20 00:40:08 +0700

    Update security

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -17,9 +16,6 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser({limit: '5mb'}));  
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
